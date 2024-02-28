18f599303b3459a0e40bfc837d25151e53968983
Danielle van Os <dvanos@outlook.com.au>
2024-02-28 12:50:45 +1100

    completed project

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -15,9 +16,49 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  const DicePage({Key? key}) : super(key: key);
+
   @override
-  Widget build(BuildContext context) {
-    return Container();
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 5; 
+
+  void changeDiceNumbers(){
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+});
+  }
+
+  @override
+  Widget build(BuildContext context) {   
+
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: TextButton(
+              child: Image.asset('images/dice$leftDiceNumber.png'),
+              onPressed: (){
+                  changeDiceNumbers();
+              },
+              ),
+          ),
+          Expanded(
+            child: TextButton(
+              child: Image.asset('images/dice$rightDiceNumber.png'),
+              onPressed: () {
+                  changeDiceNumbers();
+              },
+              ),
+          )
+        ],
+      ),
+    );
   }
 }
+
